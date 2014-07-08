f3d4fc29e8d5930c8c688873d96c0fbad070a85a
Katlyn Gabor <katlyngabor@gmail.com>
2014-07-08 15:22:18 -0400

    fixed merge conflicts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-function addText(){
-    var input = document.getElementById('input').value;
-    //document.getElementById('do').innerHTML = input;
-    var element=document.createElement("P");
-    var textinput=document.createTextNode(input);
-    element.appendChild(textinput);
-    document.getElementById('todo').appendChild(element);
-    
-
-$('#input').val('');
-
- }
-
-
-var Item = function(options){
-	var options = options || {};
-	this.text = options.text;
-	this.checked = function (){
-		if(options.checked = true)
-			options.completed=true;
-
-	};
-
-}
-=======
 	$(document).ready( function() {		
 	
 		$('#addToList').click( function() {
@@ -49,4 +23,3 @@ var Item = function(options){
 			return false;		
 		});  
 	});
->>>>>>> origin/thachbranch
