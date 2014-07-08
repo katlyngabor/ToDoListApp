a64d75498f5454d96bb671926ed93b460df07863
Katlyn Gabor <katlyngabor@gmail.com>
2014-07-08 14:16:54 -0400

    trying to fix input

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('The Iron Yard Rocks');
+function addText(){
+    var input = document.getElementById('input').value;
+    //document.getElementById('do').innerHTML = input;
+    var element=document.createElement("P");
+    var textinput=document.createTextNode(input);
+    element.appendChild(textinput);
+    document.getElementById('todo').appendChild(element);
+    
+
+
+	// $('#buttonclick').on('click', function(){
+ //    	document.getElementById('input').val('');
+	// });
+
+
+ }
+
+
