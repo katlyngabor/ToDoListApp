0ed9f147e239e1a8da60758ce7bc5d64ba06c8f0
thachmle <thachmle@aol.com>
2014-07-08 15:11:53 -0400

    styling and buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+	$(document).ready( function() {		
+	
+		$('#addToList').click( function() {
+			var todoDescription = $('#listInfo').val();
+			
+			$('.list').prepend('<div class="todo">'
+				+ '<div>'
+				//making new classes inside js 
+					+ '<input type="checkbox" class="checkingList" name="checking list"/>'
+					+ '</div>'
+					+ '<div class="listInfo">'
+					+ todoDescription
+					+ '</div>'
+			+'</div>');
+			
+			$('#textArea')[0].reset();
+			
+			$('.checkingList').unbind('click');
+			$('.checkingList').click( function() {
+				var todo = $(this).parent().parent();
+				todo.toggleClass('checked');
+			});
+			return false;		
+		});  
+	});
